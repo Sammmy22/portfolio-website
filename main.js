cf5ce5516ad39df08a51ede37aa18c422d359764
Sammmy22 <sambhavc225@outlook.com>
2023-08-06 00:04:08 +0530

    Fixed navbar color

---
+++ main.js
@@ -0,0 +1,7 @@
+const barBtn = document.getElementById("bar-btn");
+
+barBtn.addEventListener("click", () => {
+  const navbar = document.getElementById("navbar");
+
+  navbar.classList.toggle("bg-dark");
+});
