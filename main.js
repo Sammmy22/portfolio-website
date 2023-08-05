fbd8bcbdb2afd4359b5327464d17c48ebf8af2c0
Sammmy22 <sambhavc225@outlook.com>
2023-08-06 01:43:49 +0530

    Added all pages

--- main.js
+++
@@ -1,7 +0,0 @@
-const barBtn = document.getElementById("bar-btn");
-
-barBtn.addEventListener("click", () => {
-  const navbar = document.getElementById("navbar");
-
-  navbar.classList.toggle("bg-dark");
-});
