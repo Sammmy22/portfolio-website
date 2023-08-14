b8b8e16772212f3b6a8c73696cd0f5df6ae91811
Sammmy22 <sambhavc225@outlook.com>
2023-08-14 13:04:02 +0530

    Making the website single paged

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const scriptURL = "YOUR SCRIPT URL";
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwY96I_SFXuOmUjkfbw3FNTZhAbkLWw2LXxj_-zgDkbwTy_NCsCApNa71IlLceJFq_D/exec";
 const form = document.forms["contactForm"];
 
 form.addEventListener("submit", (e) => {
@@ -9,3 +10,26 @@ form.addEventListener("submit", (e) => {
 
   form.reset();
 });
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
